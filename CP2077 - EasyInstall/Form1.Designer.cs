37a0a29d7b84da9918f66bd2fc83edf75426acf8
Zenek <35767830+Zenek-Hastro@users.noreply.github.com>
2020-12-17 11:49:45 +0100

    changed buttons text, added tooltips, UI changed

--- CP2077 - EasyInstall/Form1.Designer.cs
+++ CP2077 - EasyInstall/Form1.Designer.cs
@@ -34,6 +34,7 @@ namespace CP2077___EasyInstall
             this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
             this.btnAbout = new MetroFramework.Controls.MetroLink();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnUpdate = new MetroFramework.Controls.MetroButton();
             this.cbAntialiasing = new MetroFramework.Controls.MetroCheckBox();
             this.cbSkipStartMenu = new MetroFramework.Controls.MetroCheckBox();
             this.cbRemovePedestrians = new MetroFramework.Controls.MetroCheckBox();
@@ -47,7 +48,20 @@ namespace CP2077___EasyInstall
             this.cbSpectre = new MetroFramework.Controls.MetroCheckBox();
             this.cbSMT = new MetroFramework.Controls.MetroCheckBox();
             this.cbAVX = new MetroFramework.Controls.MetroCheckBox();
-            this.btnUpdate = new MetroFramework.Controls.MetroButton();
+            this.tt_save = new MetroFramework.Components.MetroToolTip();
+            this.tt_checkUpdate = new MetroFramework.Components.MetroToolTip();
+            this.tt_json = new MetroFramework.Components.MetroToolTip();
+            this.tt_selectPath = new MetroFramework.Components.MetroToolTip();
+            this.tt_avx = new MetroFramework.Components.MetroToolTip();
+            this.tt_smt = new MetroFramework.Components.MetroToolTip();
+            this.tt_spectre = new MetroFramework.Components.MetroToolTip();
+            this.tt_async = new MetroFramework.Components.MetroToolTip();
+            this.tt_skip = new MetroFramework.Components.MetroToolTip();
+            this.tt_memory = new MetroFramework.Components.MetroToolTip();
+            this.tt_debugger = new MetroFramework.Components.MetroToolTip();
+            this.tt_vinput = new MetroFramework.Components.MetroToolTip();
+            this.tt_pedestrians = new MetroFramework.Components.MetroToolTip();
+            this.tt_aliasing = new MetroFramework.Components.MetroToolTip();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,6 +73,7 @@ namespace CP2077___EasyInstall
             this.btnMain.TabIndex = 0;
             this.btnMain.Text = "Select Path To Cyberpunk 2077 Main Directory";
             this.btnMain.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.tt_selectPath.SetToolTip(this.btnMain, "Select Path to your main CP 2077 Directory");
             this.btnMain.UseSelectable = true;
             this.btnMain.Click += new System.EventHandler(this.btnMain_Click);
             // 
@@ -66,7 +81,7 @@ namespace CP2077___EasyInstall
             // 
             this.metroLabel1.AutoSize = true;
             this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Small;
-            this.metroLabel1.Location = new System.Drawing.Point(15, 438);
+            this.metroLabel1.Location = new System.Drawing.Point(6, 386);
             this.metroLabel1.Name = "metroLabel1";
             this.metroLabel1.Size = new System.Drawing.Size(156, 15);
             this.metroLabel1.TabIndex = 1;
@@ -74,7 +89,7 @@ namespace CP2077___EasyInstall
             // 
             // btnAbout
             // 
-            this.btnAbout.Location = new System.Drawing.Point(340, 439);
+            this.btnAbout.Location = new System.Drawing.Point(347, 386);
             this.btnAbout.Name = "btnAbout";
             this.btnAbout.Size = new System.Drawing.Size(72, 15);
             this.btnAbout.TabIndex = 2;
@@ -84,13 +99,10 @@ namespace CP2077___EasyInstall
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.btnUpdate);
             this.groupBox1.Controls.Add(this.cbAntialiasing);
             this.groupBox1.Controls.Add(this.cbSkipStartMenu);
             this.groupBox1.Controls.Add(this.cbRemovePedestrians);
             this.groupBox1.Controls.Add(this.cbAsyncCompute);
-            this.groupBox1.Controls.Add(this.btnSettings);
-            this.groupBox1.Controls.Add(this.btnSave);
             this.groupBox1.Controls.Add(this.metroLabel2);
             this.groupBox1.Controls.Add(this.cbVInput);
             this.groupBox1.Controls.Add(this.cbDebug);
@@ -100,10 +112,21 @@ namespace CP2077___EasyInstall
             this.groupBox1.Controls.Add(this.cbAVX);
             this.groupBox1.Location = new System.Drawing.Point(24, 141);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(388, 292);
+            this.groupBox1.Size = new System.Drawing.Size(388, 173);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Settings:";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(237, 357);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(175, 23);
+            this.btnUpdate.TabIndex = 19;
+            this.btnUpdate.Text = "Check Update";
+            this.tt_checkUpdate.SetToolTip(this.btnUpdate, "Download and install the last patch version available");
+            this.btnUpdate.UseSelectable = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // cbAntialiasing
             // 
@@ -114,6 +137,7 @@ namespace CP2077___EasyInstall
             this.cbAntialiasing.Size = new System.Drawing.Size(143, 19);
             this.cbAntialiasing.TabIndex = 18;
             this.cbAntialiasing.Text = "Disable Antialiasing";
+            this.tt_aliasing.SetToolTip(this.cbAntialiasing, "Disables antialiasing, not recommended but you do what you want!");
             this.cbAntialiasing.UseSelectable = true;
             // 
             // cbSkipStartMenu
@@ -127,6 +151,7 @@ namespace CP2077___EasyInstall
             this.cbSkipStartMenu.Size = new System.Drawing.Size(123, 19);
             this.cbSkipStartMenu.TabIndex = 17;
             this.cbSkipStartMenu.Text = "Skip Start Menu";
+            this.tt_skip.SetToolTip(this.cbSkipStartMenu, "Skips the menu asking you to press space bar to continue (Breaching...)");
             this.cbSkipStartMenu.UseSelectable = true;
             // 
             // cbRemovePedestrians
@@ -138,6 +163,7 @@ namespace CP2077___EasyInstall
             this.cbRemovePedestrians.Size = new System.Drawing.Size(147, 19);
             this.cbRemovePedestrians.TabIndex = 16;
             this.cbRemovePedestrians.Text = "Remove Pedestrians";
+            this.tt_pedestrians.SetToolTip(this.cbRemovePedestrians, "Removes most of the pedestrians and traffic");
             this.cbRemovePedestrians.UseSelectable = true;
             // 
             // cbAsyncCompute
@@ -149,25 +175,29 @@ namespace CP2077___EasyInstall
             this.cbAsyncCompute.Size = new System.Drawing.Size(122, 19);
             this.cbAsyncCompute.TabIndex = 15;
             this.cbAsyncCompute.Text = "Async Compute";
+            this.tt_async.SetToolTip(this.cbAsyncCompute, "Disables async compute, this can give a boost on older GPUs ( nvidia 10xx series " +
+        "for example)");
             this.cbAsyncCompute.UseSelectable = true;
             // 
             // btnSettings
             // 
-            this.btnSettings.Location = new System.Drawing.Point(105, 249);
+            this.btnSettings.Location = new System.Drawing.Point(22, 357);
             this.btnSettings.Name = "btnSettings";
             this.btnSettings.Size = new System.Drawing.Size(179, 23);
             this.btnSettings.TabIndex = 14;
             this.btnSettings.Text = "Open Json settings";
+            this.tt_json.SetToolTip(this.btnSettings, "Open JSON settings, for manual editing");
             this.btnSettings.UseSelectable = true;
             this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(105, 220);
+            this.btnSave.Location = new System.Drawing.Point(22, 320);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(179, 23);
+            this.btnSave.Size = new System.Drawing.Size(390, 31);
             this.btnSave.TabIndex = 13;
             this.btnSave.Text = "Save";
+            this.tt_save.SetToolTip(this.btnSave, "Save your currently settings");
             this.btnSave.UseSelectable = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
@@ -191,6 +221,7 @@ namespace CP2077___EasyInstall
             this.cbVInput.Size = new System.Drawing.Size(102, 19);
             this.cbVInput.TabIndex = 11;
             this.cbVInput.Text = "Virtual Input";
+            this.tt_vinput.SetToolTip(this.cbVInput, "Enables the use of more gamepads (such as the Steam gamepad)");
             this.cbVInput.UseSelectable = true;
             // 
             // cbDebug
@@ -202,6 +233,7 @@ namespace CP2077___EasyInstall
             this.cbDebug.Size = new System.Drawing.Size(86, 19);
             this.cbDebug.TabIndex = 10;
             this.cbDebug.Text = "Debugger";
+            this.tt_debugger.SetToolTip(this.cbDebug, "Enables the debug menus in game so you can cheat, investigate...");
             this.cbDebug.UseSelectable = true;
             // 
             // cbMemoryPool
@@ -215,6 +247,7 @@ namespace CP2077___EasyInstall
             this.cbMemoryPool.Size = new System.Drawing.Size(107, 19);
             this.cbMemoryPool.TabIndex = 9;
             this.cbMemoryPool.Text = "Memory Pool";
+            this.tt_memory.SetToolTip(this.cbMemoryPool, "Improves memory usage, can improve performance on some configurations.");
             this.cbMemoryPool.UseSelectable = true;
             // 
             // cbSpectre
@@ -228,6 +261,7 @@ namespace CP2077___EasyInstall
             this.cbSpectre.Size = new System.Drawing.Size(70, 19);
             this.cbSpectre.TabIndex = 8;
             this.cbSpectre.Text = "Spectre";
+            this.tt_spectre.SetToolTip(this.cbSpectre, "Removes spectre mitigation to improve performance on all configurations.");
             this.cbSpectre.UseSelectable = true;
             // 
             // cbSMT
@@ -241,6 +275,7 @@ namespace CP2077___EasyInstall
             this.cbSMT.Size = new System.Drawing.Size(52, 19);
             this.cbSMT.TabIndex = 7;
             this.cbSMT.Text = "SMT";
+            this.tt_smt.SetToolTip(this.cbSMT, "Improves performance on AMD CPUs by enabling all cores.");
             this.cbSMT.UseSelectable = true;
             // 
             // cbAVX
@@ -256,31 +291,109 @@ namespace CP2077___EasyInstall
             this.cbAVX.TabIndex = 6;
             this.cbAVX.Tag = "";
             this.cbAVX.Text = "AVX";
+            this.tt_avx.SetToolTip(this.cbAVX, " Fixes a crash when playing the game with a CPU that does not support AVX");
             this.cbAVX.UseSelectable = true;
             // 
-            // btnUpdate
+            // tt_save
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(105, 191);
-            this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(179, 23);
-            this.btnUpdate.TabIndex = 19;
-            this.btnUpdate.Text = "Update";
-            this.btnUpdate.UseSelectable = true;
-            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            this.tt_save.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tt_save.StyleManager = null;
+            this.tt_save.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // tt_checkUpdate
+            // 
+            this.tt_checkUpdate.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tt_checkUpdate.StyleManager = null;
+            this.tt_checkUpdate.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // tt_json
+            // 
+            this.tt_json.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tt_json.StyleManager = null;
+            this.tt_json.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // tt_selectPath
+            // 
+            this.tt_selectPath.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tt_selectPath.StyleManager = null;
+            this.tt_selectPath.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // tt_avx
+            // 
+            this.tt_avx.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tt_avx.StyleManager = null;
+            this.tt_avx.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // tt_smt
+            // 
+            this.tt_smt.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tt_smt.StyleManager = null;
+            this.tt_smt.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // tt_spectre
+            // 
+            this.tt_spectre.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tt_spectre.StyleManager = null;
+            this.tt_spectre.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // tt_async
+            // 
+            this.tt_async.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tt_async.StyleManager = null;
+            this.tt_async.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // tt_skip
+            // 
+            this.tt_skip.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tt_skip.StyleManager = null;
+            this.tt_skip.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // tt_memory
+            // 
+            this.tt_memory.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tt_memory.StyleManager = null;
+            this.tt_memory.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // tt_debugger
+            // 
+            this.tt_debugger.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tt_debugger.StyleManager = null;
+            this.tt_debugger.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // tt_vinput
+            // 
+            this.tt_vinput.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tt_vinput.StyleManager = null;
+            this.tt_vinput.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // tt_pedestrians
+            // 
+            this.tt_pedestrians.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tt_pedestrians.StyleManager = null;
+            this.tt_pedestrians.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // tt_aliasing
+            // 
+            this.tt_aliasing.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tt_aliasing.StyleManager = null;
+            this.tt_aliasing.Theme = MetroFramework.MetroThemeStyle.Light;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(435, 461);
+            this.ClientSize = new System.Drawing.Size(435, 404);
+            this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnAbout);
             this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnMain);
+            this.Controls.Add(this.btnSettings);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Style = MetroFramework.MetroColorStyle.Yellow;
-            this.Text = "CP 2077 - EasyPatcher 2.0";
+            this.Text = "CP 2077 - EasyPatcher 2.1";
             this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
@@ -310,6 +423,20 @@ namespace CP2077___EasyInstall
         private MetroFramework.Controls.MetroCheckBox cbAsyncCompute;
         private MetroFramework.Controls.MetroCheckBox cbAntialiasing;
         private MetroFramework.Controls.MetroButton btnUpdate;
+        private MetroFramework.Components.MetroToolTip tt_save;
+        private MetroFramework.Components.MetroToolTip tt_checkUpdate;
+        private MetroFramework.Components.MetroToolTip tt_json;
+        private MetroFramework.Components.MetroToolTip tt_selectPath;
+        private MetroFramework.Components.MetroToolTip tt_avx;
+        private MetroFramework.Components.MetroToolTip tt_async;
+        private MetroFramework.Components.MetroToolTip tt_spectre;
+        private MetroFramework.Components.MetroToolTip tt_smt;
+        private MetroFramework.Components.MetroToolTip tt_skip;
+        private MetroFramework.Components.MetroToolTip tt_memory;
+        private MetroFramework.Components.MetroToolTip tt_debugger;
+        private MetroFramework.Components.MetroToolTip tt_vinput;
+        private MetroFramework.Components.MetroToolTip tt_pedestrians;
+        private MetroFramework.Components.MetroToolTip tt_aliasing;
     }
 }
 
